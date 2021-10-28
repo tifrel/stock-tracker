5016ccdd5c9db25f0cbe39084add9c3a07d1ece3
tifrel <till.friesewinkel@googlemail.com>
2021-10-28 17:54:37 +0200

    Added CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,56 @@
 use chrono::prelude::*;
+use clap::clap_app;
 use yahoo::YahooConnector;
 use yahoo_finance_api as yahoo;
 
-const STOCK_SYMBOLS: [&str; 5] = ["MSFT", "GOOG", "AAPL", "UBER", "IBM"];
 mod stocks;
 mod util;
 use stocks::*;
 
+struct Args {
+    from: DateTime<Utc>,
+    symbols: Vec<String>,
+}
+
+macro_rules! exit {
+    ($code:expr, $template:tt, $($tt:tt)*) => {{
+        eprintln!($template, $($tt)*);
+        std::process::exit($code);
+    }};
+}
+
+fn init() -> Args {
+    let matches = clap_app!(my_app =>
+        (version: "0.1.0")
+        (author: "Till Friesewinkel [till.friesewinkel@gmail.com]")
+        (about: "Fetches stock prices from the Yahoo API")
+        (@arg from: +required "Starting date in %Y-%m-%d format")
+        (@arg symbols: +required "Ticker symbols for the stocks to fetch")
+    )
+    .get_matches();
+
+    let symbols = matches
+        .value_of("symbols")
+        .unwrap()
+        .split(",")
+        .map(|s| s.to_string())
+        .collect();
+
+    let from = match DateTime::parse_from_rfc3339(matches.value_of("from").unwrap()) {
+        Err(e) => {
+            exit!(1, "Failed to parse start date: {}", e)
+        }
+        Ok(dt) => dt.with_timezone(&Utc),
+    };
+
+    Args { from, symbols }
+}
+
 fn main() {
-    let from = Utc.ymd(2021, 9, 1).and_hms(0, 0, 0);
+    let args = init();
     let connector = YahooConnector::new();
-    for symbol in STOCK_SYMBOLS {
-        match StockInfo::fetch_since(&connector, symbol, from) {
+    for symbol in args.symbols {
+        match StockInfo::fetch_since(&connector, &symbol, args.from) {
             Err(err) => eprintln!("Fetch failed: {} (\"{}\")", err, symbol),
             Ok(stock) => println!("{}", stock.fmt_csv()),
         };
